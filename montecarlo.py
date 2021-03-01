e2da543f9032c0fdc632527e30f76a3f686e328f
kawatmp <k.kawasa@gmail.com>
2021-03-01 21:11:47 +0900

    --allow-empty-message

--- montecarlo.py
+++ montecarlo.py
@@ -1,7 +1,9 @@
-# モンテカルロシミュレーションで円周率を求める
+# モンテカルロシミュレーション
 import random
+import math
 
-N = 10000000   # 繰り返し回数
+# 円周率を求める
+N = 1000000   # 繰り返し回数
 cnt = 0
 for i in range(N):
   x = random.random() # 0～1
@@ -10,3 +12,26 @@ for i in range(N):
      cnt+=1
 pi = 4 * cnt / N
 print(pi)
+
+# シュタイナー木問題（線分が最小になる点を求める）
+
+# 正方形の対角を結んだ場合(4つの線分)
+distance = 2 * math.sqrt(2)
+print(distance)
+# 5つの線分の場合
+m1 = n1 = m2 = n2 = 0
+for i in range(N):
+  x1 = random.random()
+  y1 = random.random()
+  x2 = random.random()
+  y2 = random.random()
+  d_total = math.sqrt((x1-0)**2+(1-y1)**2) + math.sqrt((1-x1)**2+(1-y1)**2) + math.sqrt((x2)**2+(y2)**2) + math.sqrt((1-x2)**2+(y2)**2) + math.sqrt((x1-x2)**2+(y1-y2)**2)
+  if d_total < distance:
+    distance = d_total
+    m1 = x1
+    n1 = y1
+    m2 = x2
+    n2 = y2
+print("min distance is " + str(distance))
+print("pos1 : " + str(m1) + " , " + str(n1))
+print("pos2 : " + str(m2) + " , " + str(n2))
